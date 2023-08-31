6453d49a2f901e49c8cf6b33a39fd0c7a3a1b5fb
Shrikant Jamale <shrikantj0794@gmail.com>
2023-08-31 19:46:38 +0530

    Template Literals in JSX

--- project2/src/index.js
+++ project2/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const fname = 'shrikant'
+const fname = 'shrikant';
+const lname = 'jamale';
+
 ReactDOM.render(
     <>
     <h1>my name is {fname}</h1>
@@ -9,6 +11,11 @@ ReactDOM.render(
     <p>calculation {2+3}</p> 
     <p>random No:- {Math.random()}</p>
     {/* used only Expression not statement,function, methode */}
+
+    {/* Template Literals in JSX */}
+    <p>My full name is {fname} {lname}</p>
+    <p>My full name is {fname+" "+lname}</p>
+    <p>{`my first name is ${fname} any my last name is ${lname}`}</p>  {/* Best way to use template literals */}
     </>,
     document.getElementById("root")
-)
+) 
