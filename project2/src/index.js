81378a5874bdb9e9cc13bef324a57b4786038fe4
Shrikant Jamale <shrikantj0794@gmail.com>
2023-08-31 19:24:08 +0530

    javascript expression

--- project2/src/index.js
+++ project2/src/index.js
@@ -4,7 +4,11 @@ import ReactDOM from 'react-dom';
 const fname = 'shrikant'
 ReactDOM.render(
     <>
-    <h1>my name is {fname}</h1>, 
+    <h1>my name is {fname}</h1>
+    <p>calculation 2+3</p> 
+    <p>calculation {2+3}</p> 
+    <p>random No:- {Math.random()}</p>
+    {/* used only Expression not statement,function, methode */}
     </>,
     document.getElementById("root")
 )
