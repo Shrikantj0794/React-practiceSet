f12bee5d3b424acb67cab2bc8f552de02a827de6
Shrikant Jamale <shrikantj0794@gmail.com>
2023-08-31 20:02:39 +0530

    Display Current Date and Time

--- project2/src/index.js
+++ project2/src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 const fname = 'shrikant';
 const lname = 'jamale';
 
+const CurrDate = new Date().toLocaleDateString();
+const CurrTime = new Date().toLocaleTimeString();
 ReactDOM.render(
     <>
     <h1>my name is {fname}</h1>
@@ -16,6 +18,10 @@ ReactDOM.render(
     <p>My full name is {fname} {lname}</p>
     <p>My full name is {fname+" "+lname}</p>
     <p>{`my first name is ${fname} any my last name is ${lname}`}</p>  {/* Best way to use template literals */}
+
+    {/* Display Current Date and Time */}
+    <p>Current Date : {CurrDate}</p>
+    <p>Current Time : {CurrTime}</p>
     </>,
     document.getElementById("root")
 ) 
