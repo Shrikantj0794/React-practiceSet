e38ff9acc5242c8d3500a81cc209e9e5fab39772
Shrikant Jamale <shrikantj0794@gmail.com>
2023-09-01 12:33:00 +0530

    adding css

--- project2/src/index.js
+++ project2/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'
 
 const fname = 'shrikant';
 const lname = 'jamale';
@@ -12,26 +13,27 @@ const image2 = 'https://picsum.photos/220/300'
 const image3 = 'https://picsum.photos/230/300'
 ReactDOM.render(
     <>
-    <h1>my name is {fname}</h1>
-    <p>calculation 2+3</p> 
+    <h1 className='size'>my name is {fname}</h1>
+    <p className='size'>calculation 2+3</p> 
     <p>calculation {2+3}</p> 
-    <p>random No:- {Math.random()}</p>
+    <p className='size'>random No:- {Math.random()}</p>
     {/* used only Expression not statement,function, methode */}
 
     {/* Template Literals in JSX */}
-    <p>My full name is {fname} {lname}</p>
+    <p className='size'>My full name is {fname} {lname}</p>
     <p>My full name is {fname+" "+lname}</p>
     <p>{`my first name is ${fname} any my last name is ${lname}`}</p>  {/* Best way to use template literals */}
 
     {/* Display Current Date and Time */}
-    <p>Current Date : {CurrDate}</p>
+    <p className='size'>Current Date : {CurrDate}</p>
     <p>Current Time : {CurrTime}</p>
     <p>Current date: {new Date().toLocaleDateString()}</p>
 
     {/* Attributes in ReactJS */}
-    <p contentEditable ="true">Currently I am pursing B.Tech in wagholi</p>
+    <p className='size' contentEditable ="true">Currently I am pursing B.Tech in wagholi</p>
 
     {/* img in react */}
+    <h2 className='style size'> Random Images</h2>
     <img src={image1} alt=''/>
     <img src={image2} alt=''/>
     <img src={image3} alt=''/>
