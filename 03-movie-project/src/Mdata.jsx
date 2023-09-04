b9e181b0518976e6a7f20fa78305cef509092e0c
Shrikant Jamale <shrikantj0794@gmail.com>
2023-09-04 18:18:36 +0530

    array added

---
+++ 03-movie-project/src/Mdata.jsx
@@ -0,0 +1,18 @@
+const Mdata = [
+    {Imgsrc: 'https://assets-in.bmscdn.com/discovery-catalog/events/et00360172-kwkpmhbqnu-landscape.jpg',
+    title: 'Bro',
+    Mlink: 'https://assets-in.bmscdn.com/discovery-catalog/events/et00360172-kwkpmhbqnu-landscape.jpg'
+    },
+
+    {Imgsrc: 'https://images.news18.com/ibnlive/uploads/2016/04/raees1.jpg?im=FitAndFill=(1200,675)?im=FitAndFill,width=1200,height=675',
+    title: 'Raees',
+    Mlink: 'https://images.news18.com/ibnlive/uploads/2016/04/raees1.jpg?im=FitAndFill=(1200,675)?im=FitAndFill,width=1200,height=675',
+    },
+
+    {Imgsrc: 'https://www.sarkariexam.com/wp-content/uploads/2023/07/OMG-2-Movie-Release-Date-and-Time-2023-Countdown-Cast-Trailer-and-More.jpg',
+    title: 'OMG',
+    Mlink: 'https://www.sarkariexam.com/wp-content/uploads/2023/07/OMG-2-Movie-Release-Date-and-Time-2023-Countdown-Cast-Trailer-and-More.jpg',
+    }
+]
+
+export default Mdata;
--- 03-movie-project/src/index.js
+++ 03-movie-project/src/index.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Card from './Cards'
+import Mdata from './Mdata'
 
 ReactDOM.render(
     <>
-    <Card Imgsrc='https://assets-in.bmscdn.com/discovery-catalog/events/et00360172-kwkpmhbqnu-landscape.jpg'
-        title='Bro'
-        Mlink='https://assets-in.bmscdn.com/discovery-catalog/events/et00360172-kwkpmhbqnu-landscape.jpg'
+    <Card Imgsrc={Mdata[0].Imgsrc}
+    title={Mdata[0].title}
+    Mlink={Mdata[0].Mlink}
     />
-    <Card Imgsrc='https://images.news18.com/ibnlive/uploads/2016/04/raees1.jpg?im=FitAndFill=(1200,675)?im=FitAndFill,width=1200,height=675'
-        title='Raees'
-        Mlink='https://images.news18.com/ibnlive/uploads/2016/04/raees1.jpg?im=FitAndFill=(1200,675)?im=FitAndFill,width=1200,height=675'
+
+    <Card Imgsrc={Mdata[1].Imgsrc}
+    title={Mdata[1].title}
+    Mlink={Mdata[1].Mlink}
     />
-    <Card Imgsrc='https://www.sarkariexam.com/wp-content/uploads/2023/07/OMG-2-Movie-Release-Date-and-Time-2023-Countdown-Cast-Trailer-and-More.jpg'
-        title='OMG'
-        Mlink='https://www.sarkariexam.com/wp-content/uploads/2023/07/OMG-2-Movie-Release-Date-and-Time-2023-Countdown-Cast-Trailer-and-More.jpg'
+
+    <Card Imgsrc={Mdata[2].Imgsrc}
+    title={Mdata[2].title}
+    Mlink={Mdata[2].Mlink}
     />
     </>,
     document.getElementById('root')
