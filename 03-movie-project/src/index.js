cd8d6d1970d4bbade9a6397c07ec19b4ea1b027d
Shrikant Jamale <shrikantj0794@gmail.com>
2023-09-05 17:33:50 +0530

    css added

--- 03-movie-project/src/index.js
+++ 03-movie-project/src/index.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Card from './Cards'
-import Mdata from './Mdata'
+import Mdata from './Mdata';
+import './index.css'
 
 ReactDOM.render(
     <>
-    <Card Imgsrc={Mdata[0].Imgsrc}
+    <Card 
+    Imgsrc={Mdata[0].Imgsrc}
     title={Mdata[0].title}
     Mlink={Mdata[0].Mlink}
     />
 
-    <Card Imgsrc={Mdata[1].Imgsrc}
+    <Card 
+    Imgsrc={Mdata[1].Imgsrc}
     title={Mdata[1].title}
     Mlink={Mdata[1].Mlink}
     />
 
-    <Card Imgsrc={Mdata[2].Imgsrc}
+    <Card 
+    Imgsrc={Mdata[2].Imgsrc}
     title={Mdata[2].title}
     Mlink={Mdata[2].Mlink}
     />
