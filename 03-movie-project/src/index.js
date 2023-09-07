06a321ce3dea5fa864e546a02a878ee6e2e9a8a2
Shrikant Jamale <shrikantj0794@gmail.com>
2023-09-07 20:02:05 +0530

    create a map() method

--- 03-movie-project/src/index.js
+++ 03-movie-project/src/index.js
@@ -11,23 +11,17 @@ import './index.css'
 ReactDOM.render(
     <>
     <h1 className='heading_style'> list of top 5 netflix movies</h1>
-    <Card 
-    Imgsrc={Mdata[0].Imgsrc}
-    title={Mdata[0].title}
-    Mlink={Mdata[0].Mlink}
-    />
 
-    <Card 
-    Imgsrc={Mdata[1].Imgsrc}
-    title={Mdata[1].title}
-    Mlink={Mdata[1].Mlink}
-    />
+    {Mdata.map(function(value){
+        return(
+        <Card 
+        Imgsrc={value.Imgsrc}
+        title={value.title}
+        Mlink={value.Mlink}
+        />
+        )
+    })}
 
-    <Card 
-    Imgsrc={Mdata[2].Imgsrc}
-    title={Mdata[2].title}
-    Mlink={Mdata[2].Mlink}
-    />
     </>,
     document.getElementById('root')
 )
