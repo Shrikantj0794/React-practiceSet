0152b4ccad745601226e64b38ce0072aa13930bb
Shrikant Jamale <shrikantj0794@gmail.com>
2023-09-21 23:18:59 +0530

    added h1

--- 05-slot_machine_game/src/App.js
+++ 05-slot_machine_game/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=>{
+  return <>
+    <h1 className="heading_style">
+      Wellcome to <span style={{fontWeight: 'bold'}}>Slot machine game</span></h1>  
+  </>
 }
 
-export default App;
+export default App
