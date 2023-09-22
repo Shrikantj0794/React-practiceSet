792eab6bcecdf37276a01fa9eee4e81fb97c5154
Shrikant Jamale <shrikantj0794@gmail.com>
2023-09-22 12:46:09 +0530

    reduse code line using ternary opertor

--- 05-slot_machine_game/src/SlotMachine.js
+++ 05-slot_machine_game/src/SlotMachine.js
@@ -1,27 +1,13 @@
 import React from 'react';
 
 const SlotMachine = (props)=>{
-   let {x,y,z} = props;
-
-   
-
-    if ((x===y) && (y===z)) {
-        return <>
-            <div className="slot_inner">
-                <h1>{x}{y}{z}</h1>
-                <h1>This is Matching</h1>
-                <hr />
-            </div>
-        </>
-    }else{
-        return <>
-            <div className="slot_inner">
-                <h1>{x}{y}{z}</h1>
-                <h1>This is Not Matching</h1>
-                <hr />
-            </div>
-        </>
-    }
+    let {x,y,z} = props;
+    return <>
+    <div className="slot_inner">
+        <h1>{x}{y}{z}</h1>
+        {(x===y) && (y===z)? <h1>This is Matching</h1>: <h1>This is Not Matching</h1>}
+        <hr />
+    </div>
+    </>
 }
-
 export default SlotMachine
