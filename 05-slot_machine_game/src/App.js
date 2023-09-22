705c82eb67b2ce5f884ca63622404bb9d9987f10
Shrikant Jamale <shrikantj0794@gmail.com>
2023-09-22 11:44:35 +0530

    emojis added in vscode

--- 05-slot_machine_game/src/App.js
+++ 05-slot_machine_game/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 const App = ()=>{
   return <>
     <h1 className="heading_style">
-      Wellcome to <span style={{fontWeight: 'bold'}}>Slot machine game</span></h1>  
+     🎰 Wellcome to <span style={{fontWeight: 'bold'}}>Slot machine game</span> 🎰</h1>  
   </>
 }
 
