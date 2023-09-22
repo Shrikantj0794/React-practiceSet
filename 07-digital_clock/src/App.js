32b9eacfd2cc77f61f00a62515b0e482265cd01e
Shrikant Jamale <shrikantj0794@gmail.com>
2023-09-22 19:46:16 +0530

    geting time on clicking button

--- 07-digital_clock/src/App.js
+++ 07-digital_clock/src/App.js
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 const App =()=>{
 
   const [Ctime, stateCtime] = useState(new Date().toLocaleTimeString())
-
   const Time =()=>{
     // toLocalTimeString only show time in hours min and sec
     stateCtime(new Date().toLocaleTimeString())
