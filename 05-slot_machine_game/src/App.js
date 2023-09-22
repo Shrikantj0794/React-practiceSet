d36bae3cf75332372adef64790420024f2518b6b
Shrikant Jamale <shrikantj0794@gmail.com>
2023-09-22 12:31:13 +0530

    completed slot machine

--- 05-slot_machine_game/src/App.js
+++ 05-slot_machine_game/src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import SlotMachine from "./SlotMachine";
 
 const App = ()=>{
   return <>
     <h1 className="heading_style">
-     🎰 Wellcome to <span style={{fontWeight: 'bold'}}>Slot machine game</span> 🎰</h1>  
+     🎰 Wellcome to <span style={{fontWeight: 'bold'}}>Slot machine game</span> 🎰</h1> 
+
+     <div className="slotmachine">
+     <SlotMachine x='😄' y='😄' z='😄'/>
+     <SlotMachine x='🎅' y='😄' z=' 👍 '/>
+     <SlotMachine x='🐱' y='👎' z=' 💯 '/>
+     <SlotMachine x='🥪' y='🥪' z=' 🥪 '/>
+
+     </div>
+     
   </>
 }
 
