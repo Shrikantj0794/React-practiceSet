d5268a87a1200c0673645bcc310334cc0d390633
Shrikant Jamale <shrikantj0794@gmail.com>
2023-09-23 15:55:32 +0530

    change one time bgcolor using usestate

--- 08-events_project/src/App.js
+++ 08-events_project/src/App.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const App =()=>{
-  return <>
-  <div>
-    <h1>Background color: red</h1>
-    <button>Change bg</button>
-  </div>
-  </>
+const App = ()=>{
+  let bgcolor1 = 'red'
+  let bgcolor2 = 'blue';
+  const [color , setcolor] = useState(bgcolor1)
+  
+  const bgChange = ()=>{
+    setcolor(bgcolor2)
+  }
+  return (
+    <>
+    <div style={{backgroundColor: color}}>
+      <button onClick={bgChange}>Change Bg Color</button>
+    </div>
+    </>
+  )
 }
 
 export default App
