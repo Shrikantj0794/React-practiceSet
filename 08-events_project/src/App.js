87f3953cd93e6d6ecb51c06fb5f49a3988d7397d
Shrikant Jamale <shrikantj0794@gmail.com>
2023-09-23 16:13:04 +0530

    multiple tyems change bgcolor

--- 08-events_project/src/App.js
+++ 08-events_project/src/App.js
@@ -3,15 +3,19 @@ import React, { useState } from "react";
 const App = ()=>{
   let bgcolor1 = 'red'
   let bgcolor2 = 'blue';
+  let bgcolor3 = 'purple'
   const [color , setcolor] = useState(bgcolor1)
   
   const bgChange = ()=>{
     setcolor(bgcolor2)
   }
+  const bgChange2 = ()=>{
+    setcolor(bgcolor3)
+  }
   return (
     <>
     <div style={{backgroundColor: color}}>
-      <button onClick={bgChange}>Change Bg Color</button>
+      <button onMouseEnter={bgChange} onMouseLeave={bgChange2}>Change Bg Color</button>
     </div>
     </>
   )
