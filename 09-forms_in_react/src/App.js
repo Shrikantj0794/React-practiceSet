a54c6bb3bd9c71a4ad0bc62348aabe1acbe54776
Shrikant Jamale <shrikantj0794@gmail.com>
2023-09-25 11:50:39 +0530

    complete the project but submit btn not working

--- 09-forms_in_react/src/App.js
+++ 09-forms_in_react/src/App.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = ()=>{
+  const [name , setname] =  useState("")
+  const [fullName, setFullName]= useState()
 
   const inputEvent = (event)=>{
-    console.log(event.target.value)
-  }
+    console.log(event.target.value);
+    setname(event.target.value);
+  };
+  const onSubmit = ()=>{
+    setFullName(name)
+  };
+  
   return <>
     <div>
-      <h1>Hello</h1>
+      <h1>Hello <span className="headingname">{fullName}</span></h1>
       <input type="text" 
       placeholder="Enter your Name" 
-      onChange={inputEvent}/>
-      <button>Submit</button>
+      onChange={inputEvent}
+      value={name}
+      />
+      <button onChange={onSubmit}>Submit</button>
     </div>
   </>
 }
