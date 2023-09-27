47ff5b673d4a6f0e46c41b4defc6b0a21ce94cd2
Shrikant Jamale <shrikantj0794@gmail.com>
2023-09-27 15:44:26 +0530

    UI created

--- 11-todo-list/src/App.js
+++ 11-todo-list/src/App.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = ()=>{
+  const [inputList, setinputList] = useState('');
+
+  const itemEvent = (event)=>{
+    setinputList(event.target.value);
+  }
+
   return <>
-  
+  <div className="main_div">
+    <div className="center_div">
+      <br />
+      <h1>ToDo List</h1>
+      <br />
+
+      <input type="text" placeholder="add item" onChange={itemEvent}/>
+      <button> + </button>
+      <ol>
+        <li>{inputList}</li>
+      </ol>
+    </div>
+  </div>
   </>
 }
 
