fb67f705546961a7b6bdf7c914445f4763e4627e
Shrikant Jamale <shrikantj0794@gmail.com>
2023-09-29 12:59:34 +0530

    completed simple  project

--- 12-incrementing-and-decrementing/src/App.js
+++ 12-incrementing-and-decrementing/src/App.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = ()=>{
+
+  const [num, setnum] = useState(0);
+
+  const incNum = ()=>{
+    setnum(num+1)
+  }
+  const decNum = ()=>{
+    if (num>0){
+    setnum(num-1)
+    }else{
+      setnum(0)
+      alert('Sorry, Zero limit Reached')
+    }
+  }
+
   return<>
-  
+    <div className="main_div">
+      <div className="center_div">
+        <h1>{num}</h1>
+        <div className="btn_div">
+        <button onClick={incNum}>Increm</button>
+        <button onClick={decNum}>Decrem</button>
+        </div>
+      </div>
+    </div>
   
   </>
 }
