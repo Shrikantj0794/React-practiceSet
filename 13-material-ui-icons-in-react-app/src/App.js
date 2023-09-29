484753513b8ffcb73bf2664861b8b5b3434c2bca
Shrikant Jamale <shrikantj0794@gmail.com>
2023-09-29 18:01:49 +0530

    add material icon- pls follow official web

--- 13-material-ui-icons-in-react-app/src/App.js
+++ 13-material-ui-icons-in-react-app/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-
+import '@fontsource/roboto/300.css';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 const App = ()=>{
 
   const [num, setnum] = useState(0);
@@ -21,8 +23,8 @@ const App = ()=>{
       <div className="center_div">
         <h1>{num}</h1>
         <div className="btn_div">
-        <button onClick={incNum}>Increm</button>
-        <button onClick={decNum}>Decrem</button>
+        <button onClick={incNum}><AddIcon/></button>
+        <button onClick={decNum}><RemoveIcon/></button>
         </div>
       </div>
     </div>
