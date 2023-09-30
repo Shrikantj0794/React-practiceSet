f698e30504567b0686f057099f7d88d327a6faf9
Shrikant Jamale <shrikantj0794@gmail.com>
2023-09-30 15:59:07 +0530

    bootstrap add in react

---
+++ 15-bootstrap-in-react/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = ()=>{
+  return <>
+  {/* adding bootstrap using: npm i bootstrap */}
+  {/* and add css link and js link add in public > index.html past in head tag */}
+    <h1 className="text-primary"> adding Bootstrap in Reat</h1>
+  </>
+}
+
+export default App
