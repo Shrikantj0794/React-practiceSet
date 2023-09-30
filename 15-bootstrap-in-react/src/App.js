8feb2a067cd8744e034ffb8a0f514fc624a0f1a7
Shrikant Jamale <shrikantj0794@gmail.com>
2023-09-30 16:00:35 +0530

    add btn

--- 15-bootstrap-in-react/src/App.js
+++ 15-bootstrap-in-react/src/App.js
@@ -5,6 +5,7 @@ const App = ()=>{
   {/* adding bootstrap using: npm i bootstrap */}
   {/* and add css link and js link add in public > index.html past in head tag */}
     <h1 className="text-primary"> adding Bootstrap in Reat</h1>
+    <button className="btn btn-primary">click me</button>
   </>
 }
 
