e7221ff8a23ff8cf3440fd60ef9aa4178f9ab9a3
Shrikant Jamale <shrikantj0794@gmail.com>
2023-09-30 16:10:44 +0530

    add extension. extension provides css name

--- 15-bootstrap-in-react/src/App.js
+++ 15-bootstrap-in-react/src/App.js
@@ -6,6 +6,9 @@ const App = ()=>{
   {/* and add css link and js link add in public > index.html past in head tag */}
     <h1 className="text-primary"> adding Bootstrap in Reat</h1>
     <button className="btn btn-primary">click me</button>
+
+    {/* add one extension: IntelliSense for CSS class names in HTML.
+     Extension that provides CSS class name completion for the HTML class attribute based on the definitions found in your workspace or external files referenced through the link element.  */}
   </>
 }
 
