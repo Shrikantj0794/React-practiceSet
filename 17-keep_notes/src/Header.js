8abf2e8a6208c145c6109a8f192593a9bd1bcea7
Shrikant Jamale <shrikantj0794@gmail.com>
2023-10-02 12:17:51 +0530

    header and footer added

---
+++ 17-keep_notes/src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from './Images/logo.png';
+
+
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <img src={logo} alt="logo"  width={70} height={90}/>
+        <h1>Keep Notes</h1>
+      </div>
+    </>
+  )
+}
+
+export default Header
