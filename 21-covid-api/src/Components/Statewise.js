a7a59665a939a6ce4b67cd305826ee78b1492fae
Shrikant Jamale <shrikantj0794@gmail.com>
2023-10-03 18:10:01 +0530

    completed project

--- 21-covid-api/src/Components/Statewise.js
+++ 21-covid-api/src/Components/Statewise.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 const Statewise = () => {
+    const [data, setdata] = useState([]);
+    const fetchData = async()=>{
+        const link = await fetch('https://data.covid19india.org/data.json');
+        const actualdata = await link.json();
+        console.log(actualdata);
+        setdata(actualdata.statewise)
+    }
+    useEffect(() => {
+            fetchData();
+    }, []);
+
   return (
     <>
-      
+      <div className="container-fluid mt-5">
+        <div className="main-heading">
+            <h1 className='mb-5 text-center'>COVID 19 Statewise List</h1>
+        </div>
+        <div className="table-responsive">
+            <table className='table table-hover'> 
+                <thead className='thead-dark'>
+                    <tr>
+                        <th>State</th>
+                        <td>Confirmed</td>
+                        <td>Recovered</td>
+                        <td>Deaths</td>
+                        <td>Active</td>
+                        <td>Statecode</td>
+                    </tr>
+                </thead>
+                <tbody>
+                       {
+                        data.map((currvalue, id)=>{
+                            return (
+                            <tr key={id}>
+                            <th>{currvalue.state}</th>
+                            <td>{currvalue.confirmed}</td>
+                            <td>{currvalue.recovered}</td>
+                            <td>{currvalue.deaths}</td>
+                            <td>{currvalue.active}</td>
+                            <td>{currvalue.statecode}</td>
+                        </tr>
+                            )
+                        })
+                       }
+                </tbody>
+            </table>
+        </div>
+      </div>
     </>
   )
 }
