e67d40713d043578059b4f3cf9906372eed8d8be
Shrikant Jamale <shrikantj0794@gmail.com>
2023-10-07 17:12:18 +0530

    redirect render on home page in case of creating Error page

--- 23-reat-router/src/App.js
+++ 23-reat-router/src/App.js
@@ -1,12 +1,13 @@
 // App.js
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home';
 import About from './Components/About';
 import Navbar from './Components/Navbar';
 import User from './Components/User';
 import Search from './Components/Search';
-import Error from './Components/Error';
+// import Error from './Components/Error';
 
 //import react router
 import {
@@ -26,7 +27,11 @@ function App() {
       <Route exact path="/Navbar" element={<Navbar/>}/>
       <Route exact path="/User/:fname/:lname" element={<User/>}/>
       <Route exact path="/Search" element={<Search/>}/>
-      <Route exact path='*' element={<Error/>}/> {/* add path= '*' */}
+      {/* add path= '*' */}
+      {/* <Route exact path='*' element={<Error/>}/> */}
+
+      <Route path="*" element={<Navigate to ="/" />}/>
+      {/* remove error page when page not found then redirect render on home page */}
     </Routes>
     </Router>
     </>
