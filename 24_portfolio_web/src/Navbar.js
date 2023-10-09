414225cdf3c776bff600f4cd6d8ca79c9bb18425
Shrikant Jamale <shrikantj0794@gmail.com>
2023-10-09 12:16:01 +0530

    create navbar

--- 24_portfolio_web/src/Navbar.js
+++ 24_portfolio_web/src/Navbar.js
@@ -4,38 +4,39 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/"><b>Shrikant Jamale</b></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/"><b>Shrikant Jamale</b></a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link className="nav-link active" activeClassname='page_active' aria-current="page" to="/">Home</Link>
         </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="/about">About</Link>
+        <li className="nav-item">
+          <Link className="nav-link" activeClassname='page_active'  to="/about">About</Link>
         </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="/contact">Contact</Link>
+        <li className="nav-item">
+          <Link className="nav-link" activeClassname='page_active' to="/contact">Contact</Link>
         </li>
 
-        <li class="nav-item dropdown">
-          <Link class="nav-link dropdown-toggle" to="/projects" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown">
+          <Link className="nav-link dropdown-toggle" activeClassname='page_active' to="/projects" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Projects
           </Link>
-          <ul class="dropdown-menu">
-          <li><Link class="dropdown-item" to="/react">React</Link></li>
-            <li><Link class="dropdown-item" to="/javascript">JavaScript</Link></li>
-            <li><Link class="dropdown-item" to="/HtmlCss">HTML/CSS</Link></li>
+          <ul className="dropdown-menu">
+
+          <li><Link className="dropdown-item" to="/react">React</Link></li>
+            <li><Link className="dropdown-item" to="/javascript">JavaScript</Link></li>
+            <li><Link className="dropdown-item" to="/HtmlCss">HTML/CSS</Link></li>
             </ul>
         </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
   </div>
