a7554e29a050886d3f41fcc9649104513ec7d4db
Shrikant Jamale <shrikantj0794@gmail.com>
2023-10-15 16:47:17 +0530

    deleted all unused file and write all description of projects

--- 24_portfolio_web/src/api/Project_data.js
+++ 24_portfolio_web/src/api/Project_data.js
@@ -3,7 +3,7 @@ const Project_data = [
         id:'1',
         imgsrc:'images/htmlcss/project_1.png',
         title:'Ecommerce web',
-        description: 'This is my first college project.',
+        description: 'Explore a online shopping excellence with our eCommerce website, meticulously crafted with HTML and CSS. ',
         visit: 'https://raisoniboys.000webhostapp.com/',
         type:'html'
     },
@@ -11,7 +11,7 @@ const Project_data = [
         id:'2',
         imgsrc:'images/htmlcss/project-2.png',
         title:'Portfolio web',
-        description:'Portfoio website creating using Html, Css and Javascript',
+        description:'Explore my portfolio website crafted using the HTML and CSS. Dive into a curated collection of my best work, beautifully presented in an intuitive and visually captivating design.',
         visit:'https://github.com/Shrikantj0794/Portfolio',
         type:'html'
     },
@@ -19,7 +19,7 @@ const Project_data = [
         id:'3',
         imgsrc:'images/javaScript/project_1.png',
         title:'Weather App',
-        description: 'Some quick example text to build on the card title and make up the bulk of the card.',
+        description: 'Experience weather like never before with our Weather App, powered by live data fetched from external APIs, User-friendly interface. ',
         visit: 'https://github.com/Shrikantj0794/30_Days-30_jsProjects/tree/main/Weather%20App',
         type: 'javascript'
     },
@@ -27,7 +27,7 @@ const Project_data = [
         id:'4',
         imgsrc:'images/javaScript/project_2.png',
         title:'Age Calculator',
-        description:'',
+        description:'Discover your age in seconds with our Age Calculator, our application powered by JavaScript.',
         visit:'https://github.com/Shrikantj0794/30_Days-30_jsProjects/tree/main/Age%20Calculator',
         type: 'javascript'
     },
@@ -35,7 +35,7 @@ const Project_data = [
         id:'5',
         imgsrc:'images/javaScript/project_3.png',
         title:'Generate-a-Random-Password',
-        description:'',
+        description:'Enhance your online security with our Generate-a-Random-Password tool, In just a single click, you can create strong, unique passwords',
         visit:'https://github.com/Shrikantj0794/30_Days-30_jsProjects/tree/main/Generate-a-Random-Password',
         type: 'javascript'
     },
@@ -43,7 +43,7 @@ const Project_data = [
         id:'6',
         imgsrc:'images/javaScript/project_4.png',
         title:'Music Player',
-        description:'',
+        description:'Play your favorite song, enjoy seamless navigation. JavaScript brings interactive features',
         visit:'https://github.com/Shrikantj0794/30_Days-30_jsProjects/tree/main/Music%20Player',
         type: 'javascript'
     },
@@ -51,7 +51,7 @@ const Project_data = [
         id:'7',
         imgsrc:'images/javaScript/project_5.png',
         title:'Notes App',
-        description:'',
+        description:'Store your thoughts and ideas with our Note App, empowered by JavaScript. Write, edit, and delete your notes, ensuring your ideas are always at your fingertips.',
         visit:'https://github.com/Shrikantj0794/30_Days-30_jsProjects/tree/main/Notes%20App',
         type: 'javascript'
     },
@@ -59,7 +59,7 @@ const Project_data = [
         id:'8',
         imgsrc:'images/javaScript/project_6.png',
         title:'QR Code Generator',
-        description:'',
+        description:'Create QR codes for URLs, contact information, and more, seamlessly and in real-time.',
         visit:'https://github.com/Shrikantj0794/30_Days-30_jsProjects/tree/main/QR%20Code%20Generator',
         type: 'javascript'
     },
@@ -67,7 +67,7 @@ const Project_data = [
         id:'9',
         imgsrc:'images/javaScript/project_7.png',
         title:'Quiz App',
-        description:'',
+        description:'Using JavaScript makes learning and entertainment easy and accessible.',
         visit:'https://github.com/Shrikantj0794/30_Days-30_jsProjects/tree/main/Quiz%20App',
         type: 'javascript'
     },
@@ -75,7 +75,7 @@ const Project_data = [
         id:'10',
         imgsrc:'images/javaScript/project_8.png',
         title:'Quote Generator',
-        description:'',
+        description:'Explore a world of wisdom with our Quote Generator, powered by APIs. ',
         visit:'https://github.com/Shrikantj0794/30_Days-30_jsProjects/tree/main/Quote%20Generator',
         type: 'javascript'
     },
@@ -83,7 +83,7 @@ const Project_data = [
         id:'11',
         imgsrc:'images/javaScript/project_9.png',
         title:'To-Do List',
-        description:'',
+        description:'Simplify your life with our To-Do List application, brought to you with the power of JavaScript. ',
         visit:'https://github.com/Shrikantj0794/30_Days-30_jsProjects/tree/main/To-Do%20List',
         type: 'javascript'
     },
@@ -91,7 +91,7 @@ const Project_data = [
         id:'12',
         imgsrc:'images/javaScript/project_10.png',
         title:'Toast Notification or Snack Bar',
-        description:'',
+        description:'Never miss a beat with our Toast Notification system, powered by JavaScript. ',
         visit:'https://github.com/Shrikantj0794/30_Days-30_jsProjects/tree/main/Toast%20Notification%20or%20Snack%20Bar',
         type: 'javascript'
     },
@@ -99,7 +99,7 @@ const Project_data = [
         id:'13',
         imgsrc:'images/reactjs/project_1.png',
         title:'Text Utils',
-        description: 'Some quick example text to build on the card title and make up the bulk of the card.',
+        description: 'Easily format and style your text—capitalize, highlight, or no of words. React.js ensures a dynamic and user-friendly text manipulation experience,',
         visit: 'https://shrikantj0794.github.io/react-textutils/',
         type:'reactjs'
     },
@@ -107,7 +107,7 @@ const Project_data = [
         id:'14',
         imgsrc:'images/reactjs/project_2.png',
         title:'React-Newsapp',
-        description:'',
+        description:'Explore the latest headlines and breaking news with our News App, enriched by the dynamic capabilities of React.js and powered by real-time news updates from APIs.',
         visit:'https://github.com/Shrikantj0794/react-newsapp',
         type:'reactjs'
     },
@@ -115,7 +115,7 @@ const Project_data = [
         id:'15',
         imgsrc:'images/reactjs/project_3.png',
         title:'movie-project',
-        description:'',
+        description:'Simple interface for movies showing in list using fetching josn file data',
         visit:'https://github.com/Shrikantj0794/React-practiceSet/tree/main/03-movie-project',
         type:'reactjs'
     },
@@ -123,7 +123,7 @@ const Project_data = [
         id:'16',
         imgsrc:'images/reactjs/project_4.png',
         title:'Keep Notes',
-        description:'',
+        description:'Store your thoughts and ideas with our Note App, empowered by React js. Write, edit, and delete your notes, ensuring your ideas are always at your fingertips.',
         visit:'https://github.com/Shrikantj0794/React-practiceSet/tree/main/17-keep_notes',
         type:'reactjs'
     },
@@ -131,7 +131,7 @@ const Project_data = [
         id:'17',
         imgsrc:'images/reactjs/project_5.png',
         title:'COVID api',
-        description:'',
+        description:'Access the latest COVID-19 statistics with our COVID Data by State application, powered by React.js and real-time data from APIs. ',
         visit:'https://github.com/Shrikantj0794/React-practiceSet/tree/main/21-covid-api',
         type:'reactjs'
     },
