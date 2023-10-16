996b3cfb40d0de12748f860b290ddf5544ddfe98
Shrikant Jamale <shrikantj0794@gmail.com>
2023-10-16 21:31:23 +0530

    create contact page

--- 24_portfolio_web/src/main_pages/Contact.js
+++ 24_portfolio_web/src/main_pages/Contact.js
@@ -1,10 +1,76 @@
 import React from 'react'
 
 const Contact = () => {
+
+  
+  const formsubmit = ()=>{
+
+  }
+
   return (
-    <div>
-      <h1>Wellcome to ccccc page</h1>
-    </div>
+    <>
+      <div className="my-3">
+        <h1 className='text-center'> Contact Us</h1>
+      </div>
+      <div className="container contact_div">
+        <div className="row">
+          <div className="col-md-6 col-10 mx-auto">
+            <form onSubmit={formsubmit}>
+            <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label">Full Name</label>
+              <input 
+              type="text" 
+              class="form-control" 
+              id="exampleFormControlInput1"
+              name='name'
+              value={}
+              onChange={InputEvent} 
+              placeholder="Enter your name" 
+              required/>
+            </div>
+            <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label">Phone No</label>
+              <input 
+              type="number" 
+              class="form-control" 
+              id="exampleFormControlInput1"
+              name='phoneNo'
+              value={}
+              onChange={InputEvent} 
+              placeholder="Mobile No" 
+              required/>
+            </div>
+            <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label">Email address</label>
+              <input 
+              type="email" 
+              class="form-control" 
+              id="exampleFormControlInput1"
+              name='email'
+              value={}
+              onChange={InputEvent} 
+              placeholder="example@gmail.com" 
+              required/>
+            </div>
+            <div class="mb-3">
+              <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+              <textarea 
+              class="form-control" 
+              id="exampleFormControlTextarea1"
+              name='msg'
+              value={}
+              onChange={InputEvent} 
+              rows="3">
+              </textarea>
+            </div>
+            <div class="col-12">
+              <button class="btn btn-primary" type="submit">Submit form</button>
+            </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- 24_portfolio_web/src/main_pages/Home.js
+++ 24_portfolio_web/src/main_pages/Home.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import Common from '../common_data/Common'
 import imglink from '../images/img2.png'
-import Projects from './Projects'
 
 const Home = () => {
   return (
     <>
     <Common 
     heading='I like to craft solid and scalable frontend products with great user experiences.'
-    visit={Projects}
-    btnText='Check Portfolio'
+    visit='/Projects'
+    btnText='Portfolio'
     src={imglink}
     />
     </>
