c1e444a9435561666b6e3c1d418f40e22b6c2054
Shrikant Jamale <shrikantj0794@gmail.com>
2023-12-29 22:02:42 +0530

    create folder

---
+++ Practice set_01/24_portfolio_web/src/main_pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Common from '../common_data/Common'
+import imglink2 from '../Home-Img/Resizefimage1.png'
+
+const About = () => {
+  return (
+    <>
+     <Common 
+    heading='Meet the Creative Mind Behind the Portfolio: Exploring Shrikant Journey'
+    visit='/contact'
+    btnText='Contact Me'
+    src={imglink2}
+    />
+    </>
+  )
+}
+
+export default About
